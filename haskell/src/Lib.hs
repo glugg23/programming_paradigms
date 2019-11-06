26e8de2ad2f104f16ce3972e273e0a27b6120b62
Max Leonhardt <m.leonhardt424@gmail.com>
2019-11-06 14:10:56 +0000

    Whitespace fixes

--- haskell/src/Lib.hs
+++ haskell/src/Lib.hs
@@ -16,5 +16,5 @@ myMap f (x:xs) = f x : myMap f xs
 myFilter :: (a -> Bool) -> [a] -> [a]
 myFilter _ [] = []
 myFilter f (x:xs)
-    | f x = x : myFilter f xs
-    | otherwise = myFilter f xs
+  | f x = x : myFilter f xs
+  | otherwise = myFilter f xs
